89e93ee568d96dd7b524ebc4c1b5fc65599026fc
codecaiine <akabrouyannickn@gmail.com>
2022-06-12 20:35:47 +0000

    Echanger les valeurs des premiers et derniers

--- exercice_1/exercice.py
+++ exercice_1/exercice.py
@@ -2,3 +2,6 @@ semaine = ["lundi", "Mardi","Mercredi",
          "Jeudi","Vendredi","Samedi", "Dimanche"]
 print(semaine)
 print(semaine[3])
+premier = semaine[0]
+semaine[0] = semaine[-1]
+dernier = semaine[-1]
