64780d011d98929307db8a9f2c0b1398fa440571
codecaiine <akabrouyannickn@gmail.com>
2022-06-12 21:32:46 +0000

    Add first function

--- exercice_2/exercice2.py
+++ exercice_2/exercice2.py
@@ -4,3 +4,11 @@ def supprimeDoublon(liste)
     if el not in initList:
       initList.append(el)
   return initList
+
+print("Entrez les valeurs .. Tapez entrer pour terminer")
+valeurListe = []
+while True:
+    valeur = input("Saisir une valeur")
+    valeurListe.append(int(valeur))
+    print("Valeur entrées:", valeurListe)
+print('La nouvelle liste est:', supprimeDoublon(valeurListe))    
