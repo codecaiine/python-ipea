905927ce448d2dc3f2e06b7e813f412ec1bc4901
codecaiine <akabrouyannickn@gmail.com>
2022-06-12 22:24:30 +0000

    Add my liste

--- exercice_3/exercice3.py
+++ exercice_3/exercice3.py
@@ -1,5 +1,9 @@
 print("Saissir la liste d'entier : ")
 maListe = []
 valeur = input("Veuillez entrer une valeur: ")
-maListe.append(int(valeur))
+while True:
+    if valeur:
+        maListe.append(int(valeur))
+    else:
+        break
 print(maListe)
