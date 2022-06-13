28c17b79bb41aec365b990450e8cd255212c9a22
codecaiine <akabrouyannickn@gmail.com>
2022-06-13 23:42:25 +0000

    Afficher les trois bonnes moyennes

--- exercice_6/exercice6.py
+++ exercice_6/exercice6.py
@@ -1,2 +1,3 @@
 moyennes=[14.84,14.14,16.22,86,85,85,14.84,13,15.85,9.99,12.04,15.03,16.22,12,84,10.20,11.03,11.03]
 moyennes.sort(reverse=True)
+print("Les trois bonnes moyennes sont :", moyennes[:3])
