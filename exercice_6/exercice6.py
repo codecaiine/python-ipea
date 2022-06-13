869f1fd2dd60471e53c6a97d9c56f2d69262e72a
codecaiine <akabrouyannickn@gmail.com>
2022-06-13 23:43:51 +0000

    Triez plus petit plus grands

--- exercice_6/exercice6.py
+++ exercice_6/exercice6.py
@@ -2,3 +2,5 @@ moyennes=[14.84,14.14,16.22,86,85,85,14.84,13,15.85,9.99,12.04,15.03,16.22,12,84
 
 moyennes.sort(reverse=True)
 print("Les trois bonnes moyennes sont :", moyennes[:3])
+
+moyennes.sort()
