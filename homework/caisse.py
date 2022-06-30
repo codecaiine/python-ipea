08b66e154785ac9ef99252d6cb1c1584c7b019c3
codecaiine <akabrouyannickn@gmail.com>
2022-06-30 20:29:40 +0000

    Calculer le nombre depiece de 100

--- homework/caisse.py
+++ homework/caisse.py
@@ -37,6 +37,9 @@ while reste > 500:
 while reste > 200:
     deux_cent +=1
     reste-= 200
+while reste > 100:
+    cent +=1
+    reste-= 100
 print('Il y a {} billets de 10.000 FCFA'.format(dix_mille))
 print('Il y a {} billets de 5.000 FCFA'.format(cinq_mille))
 print('Il y a {} billets de 2.000 FCFA'.format(deux_mille))
